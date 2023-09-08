6730a412c15ea006688eed8e66ff1ae3d7b20d1b
gaetanof <franciscogaetano1@gmail.com>
2023-09-08 11:51:17 -0300

    fixed details and added login & register view

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+const app = express();
 const publicPath = path.resolve(__dirname, './public')
 
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
     let homeHTMLPath = path.join(__dirname, './views/home.html')
     res.sendFile(homeHTMLPath);
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
 app.listen(3000, () => { console.log("Servidor escuchando puerto 3000") });
